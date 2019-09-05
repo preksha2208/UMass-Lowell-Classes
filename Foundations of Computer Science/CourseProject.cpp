2f7fec3be6e70393e13fd7460ed2509888bf8045
camknopp <camknopp@gmail.com>
2019-09-05 16:21:17 -0400

    Worked on tasks 1 and 2

--- Foundations of Computer Science/CourseProject.cpp
+++ Foundations of Computer Science/CourseProject.cpp
@@ -1,2 +1,10 @@
-// Copyright Cameron Knopp
+// Copyright Cameron Knopp 2019
 // This is my main project for Fall 2019 Foundations of Computer Science with Professor Jay McCarthy
+
+#include <iostream>
+
+/*
+Task 1: I will be using a std::list to represent an alphabet and I will be using an object to represent a character
+Task 2: I will be using an abstract class to represent a string and then I will make child-classes from that class which will allow for the building up of strings out of different elements
+
+*/
