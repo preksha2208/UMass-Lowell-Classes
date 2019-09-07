b9848ea3b86ca5efab9bea2880d4d0be06fc68ff
camknopp <camknopp@gmail.com>
2019-09-07 14:20:54 -0400

    Added Character class

--- Foundations of Computer Science/CourseProject.cpp
+++ Foundations of Computer Science/CourseProject.cpp
@@ -8,3 +8,8 @@ Task 1: I will be using a std::list to represent an alphabet and I will be using
 Task 2: I will be using an abstract class to represent a string and then I will make child-classes from that class which will allow for the building up of strings out of different elements
 
 */
+
+class Character {
+ public:
+  Character() {}
+}
