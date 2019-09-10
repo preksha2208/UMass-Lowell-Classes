dbcda4fd1b1e99302a4c5c5dadea70e7b521c754
camknopp <camknopp@gmail.com>
2019-09-10 11:36:40 -0400

    Added in the beginnings of some classes
    Still need to add in the important stuff that actually makes stuff happen

--- Foundations of Computer Science/CourseProject.cpp
+++ Foundations of Computer Science/CourseProject.cpp
@@ -1,15 +1,46 @@
 // Copyright Cameron Knopp 2019
-// This is my main project for Fall 2019 Foundations of Computer Science with Professor Jay McCarthy
-
-#include <iostream>
+// This is my course project for Fall 2019 Foundations of Computer Science with Professor Jay McCarthy
 
 /*
+TASK NOTES:
 Task 1: I will be using a std::list to represent an alphabet and I will be using an object to represent a character
 Task 2: I will be using an abstract class to represent a string and then I will make child-classes from that class which will allow for the building up of strings out of different elements
-
+Task 3: Still need to complete. Unsure of what exactly it is I am supposed to do, so I am skipping forward to task 4.
+  - I think my issue is likely that I just don't fully understand how the character and string classes are going to coursework
+  - What are some examples of what the string objects will be holding? Will it be binary, will it be full words, will it be symbols?
+Task 4: Unsure of what is meant by representing the set of states as a membership function
+  - Confused as to how the data could be held by a function, or does he mean that
+  - Need to think about what exactly the DFA class will be capable of doing
 */
 
-class Character {
+
+#include <iostream>
+#include <string>
+
+class myChar {
  public:
-  Character() {}
+  myCharacter() {}
+}
+
+class myString {
+ public:
+
+}
+
+class oneString : public myString {
+ public:
+  oneString
+
+}
+
+class DFA {
+ public:
+  DFA() {}
+
+ private:
+
+}
+
+string lexi(std::list<string> alphabet) {    // this is just a rough outline of how the function will work, need to replace string with self-created string class
+  // first sort the strings in lexicographical order, then just return the string at the given position in the list
 }
