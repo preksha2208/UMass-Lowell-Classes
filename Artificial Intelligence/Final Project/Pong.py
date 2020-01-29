f7728ddf41f5d328dfc1deabc578b211da76093a
camknopp <camknopp@gmail.com>
2020-01-29 07:36:11 -0500

    Added in first paddle

--- Artificial Intelligence/Final Project/Pong.py
+++ Artificial Intelligence/Final Project/Pong.py
@@ -6,6 +6,17 @@ wn.bgcolor("black")
 wn.setup(width = 800, height = 600)
 wn.tracer(0)
 
+#Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("circle")
+paddle_a.color("white")
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+#Paddle B
+
+#Ball
 
 #Main game loop (every game needs this)
 while True:
