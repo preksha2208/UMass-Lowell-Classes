9999fc83509932a3e78c464e8a0a07cb1758edce
camknopp <camknopp@gmail.com>
2020-02-03 21:18:54 -0500

    Added comments

--- Organization of Programming Languages/CameronKnopp_OPL_A1/cartesian.pl
+++ Organization of Programming Languages/CameronKnopp_OPL_A1/cartesian.pl
@@ -1,18 +1,11 @@
-% Code taken from https://stackoverflow.com/questions/40244936/prolog-cartesian-product-calculator?rq=1
+% Code taken from https://stackoverflow.com/a/49503900
 
-% call new with 4 parameters (so we can keep List1 in memory)
-product(L1,L2,L3):- product(L1,L2,L3,L1).
+:- use_module(library(chr)).
 
-% stop when both List1 and List2 are empty
-product(_, [], [], _).
-
-% first list is empty, recreate it and work it again with the next element of second list (and shorten memory)
-product([], [_|T2], List3, L4):-
-    product(L4, T2, List3, L4).
-
-%go through first list and always first element of second list to our answer
-product([H1|T1], [H2|T2], [[H1,H2]|T3], List4):-
-    product(T1, [H2|T2], T3, List4).
+% definition of cartesian product function
+% uses findall predicate and member predicates to find all (X,Y) where X is a member of set A and Y is a member of set B (aka cartesian product)
+product(A,B,C) :-
+    findall([X,Y],(member(X,A),member(Y,B)),C).
 
 % example input: product([1,2,3], [a,b], X).
 % example input: product([a], [a,b], X).
