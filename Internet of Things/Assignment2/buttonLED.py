4ef063ee06e52d213f138345de7642fe53273bbf
Cameron Knopp <33296196+camknopp@users.noreply.github.com>
2020-02-04 09:19:50 -0500

    Added finished Raspberry Pi code

---
+++ Internet of Things/Assignment2/buttonLED.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(21,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(18, GPIO.OUT)
+buttonstate = False
+ledOn = False
+
+while True:
+	buttonstate = GPIO.input(21)
+
+	# turn LED on if the button is pressed and the LED is off
+	if buttonstate == 1 and ledOn == 0:
+		GPIO.output(18, GPIO.HIGH)
+		time.sleep(.2)
+		ledOn = True
+	
+	# keep LED if it is already on and the button isn't being pressed
+	elif buttonstate == 0 and ledOn == 1:
+		GPIO.output(18, GPIO.HIGH)
+		time.sleep(.2)
+	
+	# turn LED off otherwise
+	else:
+		GPIO.output(18, GPIO.LOW)
+		time.sleep(.2)
+		ledOn = False
+
+GPIO.cleanup()
