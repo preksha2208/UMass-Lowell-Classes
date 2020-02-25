fafcf57bcaff70548f216d04e7845c0e601f4e08
camknopp <camknopp@gmail.com>
2020-02-24 19:25:18 -0500

    Wrote in case statement

--- Organization of Programming Languages/CameronKnopp_OPL_A2/scan.cpp
+++ Organization of Programming Languages/CameronKnopp_OPL_A2/scan.cpp
@@ -1,70 +1,150 @@
 /* Simple ad-hoc scanner for the calculator language.
     Michael L. Scott, 2008-2017.
 */
-#include <iostream>
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
-#include "ctype.h"
-#include "scan.h"
- 
-//char token_image[100];
 
-token scan() {
-    static int c = ' ';
-        /* next available char; extra (int) width accommodates EOF */
-    int i = 0;              /* index into token_image */
+#include <iostream>
+#include <ios>
+#include <cstdlib> //to exit(1)
+#include <string>
+#include <cctype>
+
+#include "scan.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::noskipws;
+using std::string;
+
+char token_image[100];
+string image;
+string getImage()
+{
+    return " \"" + image + "\"";
+}
+
+token scan()
+{
+    static char c = ' ';
+    /* next available char; extra (int) width accommodates EOF */
+    int i = 0; /* index into token_image */
 
     /* skip white space */
-    while (isspace(c)) {
-        c = getchar();
-    }
-    if (c == EOF)
-        return t_eof;
-    if (isalpha(c)) {
-        do {
-            token_image[i++] = c;  // put inputted characters into token_image
-            c = getchar();
-        } while (isalpha(c) || isdigit(c) || c == '_');
-        token_image[i] = '\0';  // add end of string at final index
-        if (!strcmp(token_image, "read"))
-            return t_read;
-        if (!strcmp(token_image, "write"))
-            return t_write;
 
-        // may use below once I change from getchar() to cin or something like that
-        /*if (token_image != "read")
-            return t_read;
-        if (token_image != "write")
-            return t_write;
-        */
-        else return t_id;
-    }
-    else if (isdigit(c)) {
-        do {
+    /*if (!cin.get(c)) return t_eof;
+    while (isspace(c)) {
+        if (!cin.get(c)) return t_eof;
+    }*/
+    do
+    {
+        if (!cin.get(c))
+            return t_eof;
+    } while (isspace(c));
+    if (isalpha(c))
+    {
+        do
+        {
             token_image[i++] = c;
-            c = getchar();
+            if (!cin.get(c))
+                break;
+        } while (isalpha(c) || isdigit(c) || c == '_');
+        token_image[i] = '\0';
+        if (token_image == string("read"))
+            return t_read;
+        else if (token_image == string("write"))
+            return t_write;
+        else if (token_image == string("do"))
+            return t_do;
+        else if (token_image == string("od"))
+            return t_od;
+        else if (token_image == string("if"))
+            return t_if;
+        else if (token_image == string("fi"))
+            return t_fi;
+        else if (token_image == string("check"))
+            return t_check;
+        else
+        {
+            image = token_image;
+            return t_id;
+        }
+        // literal = token_image;
+    }
+    else if (isdigit(c))
+    {
+        do
+        {
+            token_image[i++] = c;
+            cin.get(c);
         } while (isdigit(c));
         token_image[i] = '\0';
+        image = token_image;
         return t_literal;
-    } else switch (c) {
+    }
+    else
+        switch (c)
+        { // using switch/case
+        case '+':
+            cin.get(c);
+            return t_add;
+        case '-':
+            cin.get(c);
+            return t_sub;
+        case '*':
+            cin.get(c);
+            return t_mul;
+        case '/':
+            cin.get(c);
+            return t_div;
+        case '(':
+            cin.get(c);
+            return t_lparen;
+        case ')':
+            cin.get(c);
+            return t_rparen;
+
         case ':':
-            if ((c = getchar()) != '=') {
-                std::cerr << "error\n";
-                exit(1);
-            } else {
-                c = getchar();
+            cin.get(c);
+            if (c == '=')
+            {
+                cin.get(c);
                 return t_gets;
             }
-            break;
-        case '+': c = getchar(); return t_add;
-        case '-': c = getchar(); return t_sub;
-        case '*': c = getchar(); return t_mul;
-        case '/': c = getchar(); return t_div;
-        case '(': c = getchar(); return t_lparen;
-        case ')': c = getchar(); return t_rparen;
+            cout << "error\n";
+        case '<':
+            cin.get(c);
+            if (c == '>')
+            {
+                cin.get(c);
+                return t_notequal;
+            }
+            else if (c == '=')
+            {
+                cin.get(c);
+                return t_smallerequal;
+            }
+            else
+                return t_smaller;
+        case '>':
+            cin.get(c);
+            if (c == '=')
+            {
+                cin.get(c);
+                return t_greaterequal;
+            }
+            else
+                return t_greater;
+        case '=':
+            cin.get(c);
+            if (c == '=')
+            {
+                cin.get(c);
+                return t_equal;
+            }
+            cout << "error\n";
         default:
-            std::cerr << "error\n";
+            cout << "error\n"; //default case
             exit(1);
-    }
+        }
 }
+
