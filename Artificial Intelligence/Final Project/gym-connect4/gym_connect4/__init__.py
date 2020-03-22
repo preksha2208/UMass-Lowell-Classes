b57a4a2b8fc35f1900ade770e71c903d262fee88
camknopp <camknopp@gmail.com>
2020-03-22 13:12:25 -0400

    Got OpenAI gym directory structure correct

--- Artificial Intelligence/Final Project/gym-connect4/gym_connect4/__init__.py
+++ Artificial Intelligence/Final Project/gym-connect4/gym_connect4/__init__.py
@@ -2,5 +2,5 @@ from gym.envs.registration import register
 
 register(
     id='connect4-v0',
-    entry_point='gym_connect4.envs:connect4',
+    entry_point='gym_connect4.envs:connect4'
 )
--- Artificial Intelligence/Final Project/gym-connect4/gym_connect4/envs/connect4_env.py
+++ Artificial Intelligence/Final Project/gym-connect4/gym_connect4/envs/connect4_env.py
@@ -2,14 +2,14 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 
-class Env(gym.Env):
+class connect4(gym.Env):
   metadata = {'render.modes': ['human']}
 
   def __init__(self):
-    pass
+    print("init success")
   def step(self, action):
-    pass
+    print("step success")
   def reset(self):
-    pass
+    print("reset success")
   def render(self, mode='human', close=False):
-    pass
+    print("render success")
